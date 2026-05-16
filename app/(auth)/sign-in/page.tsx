ae30c40ea3236e1a5c34b4c5baab415162a6dbf5
Chahat Joshi <sdhrtjoshi@gmail.com>
2026-05-16 16:40:17 +0530

    Implement Auth UI & Layout

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import InputField from '@/components/forms/InputField';
+import FooterLink from '@/components/forms/FooterLink';
+
+const SignIn = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<SignInFormData>({
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        mode: 'onBlur',
+    });
+
+    const onSubmit = async (data: SignInFormData) => {
+        try {
+            console.log(data);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <>
+            <h1 className="form-title">Welcome Back</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                <InputField
+                    name="email"
+                    label="Email"
+                    placeholder="example@gmail.com"
+                    register={register}
+                    error={errors.email}
+                    validation={{
+                        required: 'Email is required',
+                        pattern: {
+                            value: /^\w+@\w+\.\w+$/,
+                            message: 'Invalid email address',
+                        },
+                    }}
+                />
+
+                <InputField
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                    type="password"
+                    register={register}
+                    error={errors.password}
+                    validation={{
+                        required: 'Password is required',
+                        minLength: {
+                            value: 8,
+                            message: 'Password must be at least 8 characters',
+                        },
+                    }}
+                />
+
+                <Button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="yellow-btn w-full mt-5"
+                >
+                    {isSubmitting ? 'Signing in...' : 'Sign In'}
+                </Button>
+
+                <FooterLink
+                    text="Don't have an account?"
+                    linkText="Sign up"
+                    href="/sign-up"
+                />
+            </form>
+        </>
+    );
+};
+
+export default SignIn;
